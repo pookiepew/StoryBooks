54149be1a3c814a711963eb19116b5a3d3e770e3
pookiepew <glensorbo@gmail.com>
2019-05-11 18:19:15 +0200

    passport/google-Oauth added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost/storybooks',
+    googleClientID: '415730312195-fh3q6bmkr271nm8tc7ibe71iugs6tups.apps.googleusercontent.com',
+    googleClientSecret: 'z2_Foobd9P54XyRLmr8_KKKW'
+}
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}))
+
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}),
+(req, res) => {
+    // Successful authentication, redirect to dashboard
+    res.redirect('/dashboard')
+})
+
+module.exports = router
