ea787f6c363300038fce23375ea7e2a8ea5a83c2
pookiepew <glensorbo@gmail.com>
2019-05-11 17:30:51 +0200

    initial express setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It works')
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
